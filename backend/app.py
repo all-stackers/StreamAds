2471f8196f4f10c80e76d77a1d4917af0227fe43
Rupesh Raut <rupeshraut99396@gmail.com>
2024-08-27 12:28:40 +0530

    create user, company, and campaign APIs

---
+++ backend/app.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from flask import Flask
+from flask_restful import Api
+from mongo_engine import db
+from flask_cors import CORS
+import os
+
+from resources.company import Company
+from resources.user import User
+from resources.campaign import Campaign, AddParticipantToCampaign
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+app.config["MONGODB_HOST"] = os.getenv("FLASK_MONGODB_URI")
+db.init_app(app)
+
+api.add_resource(Company, '/company')
+api.add_resource(User, '/user')
+api.add_resource(Campaign, '/campaign')
+api.add_resource(AddParticipantToCampaign,'/campaign/add_participant')
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5000, debug=True)
