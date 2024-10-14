d4887fb5280abd57f856455e7295af131b99c953
Rupesh Raut <rupeshraut99396@gmail.com>
2024-10-14 19:26:02 +0530

    add missing packages in requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -609,5 +609,5 @@ def retweet():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host="0.0.0.0", port=port)
