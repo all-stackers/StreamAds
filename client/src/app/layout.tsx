4487d865d5ed51b9a4a246d2e141a0e7e38452e5
Kenil Shah <kenilshah1505@gmail.com>
2024-10-14 18:32:24 +0700

    added some changes to wallet connector

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,11 +4,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import Navabar from "@/components/navbar";
-
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+
 import { Network } from "@aptos-labs/ts-sdk";
 import { PropsWithChildren } from "react";
-import { OKXWallet } from "@okwallet/aptos-wallet-adapter";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -18,18 +20,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-   const wallets = [
-    new OKXWallet(),
-  ];
+  const wallets = [new PetraWallet()];
   return (
-        <AptosWalletAdapterProvider
-   
-      
-      autoConnect={false}
-      onError={(error) => {
-    console.log("error", error);
-  }}
-    >
+       <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
   
 
     <html lang="en">
--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,67 +1,16 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-import { useWallet, WalletName } from "@aptos-labs/wallet-adapter-react";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { AptosConfig, Aptos, Network } from "@aptos-labs/ts-sdk";
 
 const Navabar = () => {
-   const { connect, disconnect, connected, account, signAndSubmitTransaction } = useWallet();
-  const [userAddress, setUserAddress] = React.useState("");
-  const [publicKey, setPublicKey] = React.useState("null");
- const handleConnect = async () => {
-   
-      // Change below to the desired wallet name instead of "Petra"
-      await connect("Petra" as WalletName<"Petra">);
-    console.log(account);
-      setUserAddress(account?.address || "");
-      
-   
-    
- };
-  const consilidate = async () => {
-    console.log("hi", account);
-  }
- 
-  const handleDisconnect = async () => {
-    try {
-      console.log(account)
-      await disconnect();
-      console.log('Disconnected from wallet');
-    } catch (error) {
-      console.error('Failed to disconnect from wallet:', error);
-    }
-  };
- 
-
-
-  
-  const createCampaign = async () => {
-    console.log("hi", account);
-    const config = new AptosConfig({ network: Network.TESTNET });
-    const aptos = new Aptos(config);
-    const modules = await aptos.getAccountModules({ accountAddress: "0xf0c37761c0d644014c98bec8255d5836f13b4120b9059a0dab21a49355dded53" });
-    console.log(modules);
-    if(account == null) {
-        throw new Error("Unable to find account to sign transaction")
-    }
-    const response = await signAndSubmitTransaction({
-      sender: account.address,
-      data: {
-        function: "0xf0c37761c0d644014c98bec8255d5836f13b4120b9059a0dab21a49355dded53::stream::distribute_rewards",
-        typeArguments: ["0x1::aptos_coin::AptosCoin"],
-        functionArguments: [1,["0xe4b14261dcfdc02456b31d0a216a1de2a706efcabc817efdd6f3f42f0480db79","0x152e85f4670b489587b8cd22022615f318733a6d6f7855a24b8af39ae64dc683"],100000000 ]
-      },
-    });
-    // if you want to wait for transaction
-   
- try {
-      await aptos.waitForTransaction({ transactionHash: response.hash });
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  
+  const { account } = useWallet();
+  console.log(account)
   const router = useRouter();
   return (
     <div className="flex px-[50px] py-[20px] justify-center">
@@ -96,19 +45,9 @@ const Navabar = () => {
             className="h-[40px] rounded-full"
           />
          
-          {/* <button onClick={() => createCampaign()}> here </button> */}
-           {connected ? (<button
-      className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 focus:outline-none"
-      onClick={handleDisconnect}
-          >
-           
-      Disconnect
-    </button>):(<button
-      className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 focus:outline-none"
-      onClick={handleConnect}
-    >
-      Connect
-    </button>)}
+        
+  <WalletSelector />
+
       
         </div>
       </div>
