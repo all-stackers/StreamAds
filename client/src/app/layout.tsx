a642bd897bc0ee8efcee50c60841ea497627637f
Kenil Shah <kenilshah1505@gmail.com>
2024-10-15 23:28:28 +0700

    added navbar and some ui changes

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 
     <html lang="en">
       <body className={inter.className}>
-        <Navabar />
+  
         <main>{children}</main>
         <Toaster />
       </body>
